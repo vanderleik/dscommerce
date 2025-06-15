0608098bf3f109893d96bd18058a557b85eca3ac
vanderleik <vanderleik@yahoo.com.br>
2025-06-15 09:49:43 -0300

    feat: Adicionando enpoint para retornar usuário logado

---
+++ src/main/java/com/api/dscommerce/controllers/UserController.java
@@ -0,0 +1,27 @@
+package com.api.dscommerce.controllers;
+
+import com.api.dscommerce.dto.UserDTO;
+import com.api.dscommerce.services.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_CLIENT')")
+    @GetMapping(value = "/me")
+    public ResponseEntity<UserDTO> getMe() {
+        return ResponseEntity.ok(service.getMe());
+    }
+
+}
---
+++ src/main/java/com/api/dscommerce/dto/UserDTO.java
@@ -0,0 +1,54 @@
+package com.api.dscommerce.dto;
+
+import com.api.dscommerce.entities.User;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDTO {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private LocalDate birthDate;
+
+    private List<String> roles = new ArrayList<>();
+
+    public UserDTO(User userEntity) {
+        id = userEntity.getId();
+        name = userEntity.getName();
+        email = userEntity.getEmail();
+        phone = userEntity.getPhone();
+        birthDate = userEntity.getBirthDate();
+        for (GrantedAuthority role : userEntity.getRoles()) {
+            roles.add(role.getAuthority());
+        }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+}
